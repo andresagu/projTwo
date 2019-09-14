4bb7f55ba6f7ceedb06c7cd8c5b5dd16eb3518b0
Andes Aguaiza <andresagu2595@gmail.com>
2019-09-14 14:01:05 -0400

    more db

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "andres",
-    password: "password123",
-    database: "classSandbox",
-    host: "127.0.0.1",
+    username: "iwyp2euedo5cxope",
+    password: "	c9mrvijpmjk24qyj",
+    database: "wz9kpuyfffg02nro",
+    host: "i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port:3306,
     dialect: "mysql"
   },
