2e2495f36c1237882fe35f04192b18d52d29a4ea
Andes Aguaiza <andresagu2595@gmail.com>
2019-09-14 13:58:36 -0400

    adding jaws

--- config/config.js
+++ config/config.js
@@ -8,18 +8,18 @@ module.exports = {
     dialect: "mysql"
   },
   test: {
-    username: "andres",
-    password: "password123",
-    database: "classSandbox",
-    host: "127.0.0.1",
+    username: "iwyp2euedo5cxope",
+    password: "	c9mrvijpmjk24qyj",
+    database: "wz9kpuyfffg02nro",
+    host: "i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port:3306,
     dialect: "mysql"
   },
   production: {
-    username: "n7hdn4tj19trkrik",
-    password: "r28h21n299b8kphx",
-    database: "gzd51lb4w0xmpqon",
-    host: "arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    username: "iwyp2euedo5cxope",
+    password: "	c9mrvijpmjk24qyj",
+    database: "wz9kpuyfffg02nro",
+    host: "i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port:3306,
     dialect: "mysql"
   }
